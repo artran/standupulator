50fbe4e9f29f5bdfc5024cd1524a48156aaf6adc
Ray Tran <rt@elixirsoftware.co.uk>
2022-12-21 10:09:10 +0000

    Update libs and add John to list of users

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::thread_rng;
 
 fn main() {
     let mut rng = thread_rng();
-    let mut y = ["Ray", "Martin", "Robin", "Huw", "Al", "Steve", "Neil", "Will"];
-    y.shuffle(&mut rng);
-    println!("Today's order:   {:?}", y);
+    let mut users = ["Ray", "Martin", "Robin", "Huw", "Al", "Steve", "Neil", "Will", "John"];
+    users.shuffle(&mut rng);
+    println!("Today's order:   {:?}", users);
 }
