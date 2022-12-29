cefa47def76bd3956269591a2d77bca7c0a9e033
Ray Tran <rt@elixirsoftware.co.uk>
2022-12-29 20:20:49 +0000

    Make it possible to read the names from a file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+use rand::{thread_rng, seq::SliceRandom};
 
 fn main() {
+    let file_result = File::open("input.txt");
+    let mut line = String::new();
+    let mut names = match file_result {
+        Ok(file) => {
+            let mut reader = io::BufReader::new(file);
+
+            let names = match reader.read_line(&mut line) {
+                Ok(_ok) => {
+                    line.split(",").map(|s| s.trim()).collect()
+                },
+                Err(_err) => get_default_names(),
+            };
+            names
+        },
+        Err(_error) => get_default_names()
+    };
+
     let mut rng = thread_rng();
-    let mut users = ["Ray", "Martin", "Robin", "Huw", "Al", "Steve", "Neil", "Will", "John"];
-    users.shuffle(&mut rng);
-    println!("Today's order:   {:?}", users);
+    names.shuffle(&mut rng);
+    println!("Today's order:   {:?}", names);
+}
+
+
+fn get_default_names() -> Vec<&'static str> {
+    vec!["Ray", "Martin", "Robin", "Huw", "Al", "Steve", "Neil", "Will", "John"]
 }
